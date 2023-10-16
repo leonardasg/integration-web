7aeae00acfc0b15215384dea7c8d05a8173f422c
leonardasg <i.grazulis@ktu.edu>
2023-10-16 13:30:01 +0300

    Update

--- resources/views/tasks/member_tasks_dashboard.blade.php
+++ resources/views/tasks/member_tasks_dashboard.blade.php
@@ -16,88 +16,99 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="card-body">
-                    @include('alerts.success')
-                    <div class="table-container">
-                        <table class="table">
-                            <thead class=" text-primary">
-                            <th>No.</th>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Type</th>
-                            <th>Points</th>
-                            <th>Date From</th>
-                            <th>Date To</th>
-                            <th>Active</th>
-                            <th>Created by</th>
-                            </thead>
-                            <tbody>
-                            @foreach($tasks as $task)
-                                <tr>
-                                    <td>{{$loop->index + 1}}</td>
-                                    <td>{{ $task->name }}</td>
-                                    <td>{{ $task->description }}</td>
-                                    <td>{{ $task->role_name }}</td>
-                                    <td class="text-center">{{ $task->points }}</td>
-                                    <td>
-                                        @if(isset($task->date_from))
-                                            {{$task->date_from}}
-                                        @else
-                                            -
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if(isset($task->date_to))
-                                            {{$task->date_to}}
-                                        @else
-                                            -
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if($task->active)
-                                            <span class="tim-icons green icon-check-2"></span>
-                                        @else
-                                            <span class="tim-icons red icon-simple-remove"></span>
-                                        @endif
-                                    </td>
-                                    <td class="purple">You</td>
-                                    <td class="text-right">
-                                        <div class="dropdown">
-                                            <button type="button" class="btn btn-link dropdown-toggle btn-icon"
-                                                    data-toggle="dropdown">
-                                                <i class="tim-icons icon-pencil"></i>
-                                            </button>
-                                            <div class="dropdown-menu dropdown-menu-right"
-                                                 aria-labelledby="dropdownMenuLink">
-                                                <a href="{{ route('task.edit', ['task' => $task]) }}"
-                                                   class="dropdown-item">Edit</a>
-
-                                                <form method="POST" action="{{ route('task.remove') }}">
-                                                    @csrf
-                                                    @method('put')
-
-                                                    <input type="hidden" name="id_task" value={{ $task->id }}>
-                                                    <button class="dropdown-item red confirm-form" type="submit" data-confirm="Are you sure you want to remove this task?">Remove</button>
-                                                </form>
-                                                @if($task->active)
-                                                    <a class="dropdown-item green" href="#" data-toggle="modal" data-target="#assign" data-task="{{$task->id}}">Assign freshman</a>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @if(!empty($task->assigned_to))
+                    @if(!empty($tasks))
+                        @include('alerts.success')
+                        <div class="table-container">
+                            <table class="table">
+                                <thead class=" text-primary">
+                                <th>No.</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Type</th>
+                                <th>Points</th>
+                                <th>Date From</th>
+                                <th>Date To</th>
+                                <th>Active</th>
+                                <th>Created by</th>
+                                </thead>
+                                <tbody>
+                                @foreach($tasks as $task)
                                     <tr>
-                                        <td colspan="10">
-                                            @include('tasks.assigned_to')
+                                        <td>{{$loop->index + 1}}</td>
+                                        <td>{{ $task->name }}</td>
+                                        <td>{{ $task->description }}</td>
+                                        <td>{{ $task->role_name }}</td>
+                                        <td class="text-center">{{ $task->points }}</td>
+                                        <td>
+                                            @if(isset($task->date_from))
+                                                {{$task->date_from}}
+                                            @else
+                                                -
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if(isset($task->date_to))
+                                                {{$task->date_to}}
+                                            @else
+                                                -
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if($task->active)
+                                                <span class="tim-icons green icon-check-2"></span>
+                                            @else
+                                                <span class="tim-icons red icon-simple-remove"></span>
+                                            @endif
+                                        </td>
+                                        <td class="purple">You</td>
+                                        <td class="text-right">
+                                            <div class="dropdown">
+                                                <button type="button" class="btn btn-link dropdown-toggle btn-icon"
+                                                        data-toggle="dropdown">
+                                                    <i class="tim-icons icon-pencil"></i>
+                                                </button>
+                                                <div class="dropdown-menu dropdown-menu-right"
+                                                     aria-labelledby="dropdownMenuLink">
+                                                    <a href="{{ route('task.edit', ['task' => $task]) }}"
+                                                       class="dropdown-item">Edit</a>
+
+                                                    <form method="POST" action="{{ route('task.remove') }}">
+                                                        @csrf
+                                                        @method('put')
+
+                                                        <input type="hidden" name="id_task" value={{ $task->id }}>
+                                                        <button class="dropdown-item red confirm-form" type="submit"
+                                                                data-confirm="Are you sure you want to remove this task?">
+                                                            Remove
+                                                        </button>
+                                                    </form>
+                                                    @if($task->active)
+                                                        <a class="dropdown-item green" href="#" data-toggle="modal"
+                                                           data-target="#assign" data-task="{{$task->id}}">Assign
+                                                            freshman</a>
+                                                    @endif
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
-                                @endif
-                            @endforeach
-                            </tbody>
-                        </table>
+                                    @if(!empty($task->assigned_to))
+                                        <tr>
+                                            <td colspan="10">
+                                                @include('tasks.assigned_to')
+                                            </td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                                </tbody>
+                            </table>
 
-                    </div>
+
+                        </div>
+                    @else
+                        <h6 class="red">{{__('You do not have any tasks yet.')}}</h6>
+                    @endif
                 </div>
             </div>
         </div>
